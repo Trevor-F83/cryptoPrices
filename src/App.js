de30f6f4149e2e8ede105a38e3e4f9feeb1dfdcc
trevorfaivre <trevorfaivre@hotmail.com>
2022-06-09 15:12:43 -0500

    routes complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 //Import route and components
 import { Route } from "react-router-dom";
@@ -9,20 +9,15 @@ import Price from "./pages/Price";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route path="/">
+        <Main />
+      </Route>
+      <Route path="/currencies">
+        <Currencies />
+      </Route>
+      <Route path="/price/:symbol">
+      <Price />
+      </Route>
     </div>
   );
 }
